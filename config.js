48a1da87cb25620dd8c2dba229dfffbf5629e136
Donpere <doumemarion@hotmail.com>
2023-12-17 17:08:45 +0100

    add .gitignore

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const config = {
     database: {
       host: '127.0.0.1',
       user: 'votre_utilisateur',
-      password: 'votre_mot_de_passe',
-      database: 'nom_de_votre_base_de_donnees',
+      password: 'boss@442442',
+      database: 'boulange-bdd',
       port: 3306
     }
   };
